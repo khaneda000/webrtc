0c5d65c70c12704cafd1851925b74b944d080a95
Kentarou Haneda <kentaro.haneda@rarejob.co.jp>
2017-06-19 13:25:30 +0900

    update

--- webrtc/AppDelegate.swift
+++ webrtc/AppDelegate.swift
@@ -16,7 +16,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        RTCInitializeSSL()
         return true
+    }
+    
+    func applicationWillTerminate(_ application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        RTCCleanupSSL()
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
@@ -35,10 +41,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
 
---
+++ webrtc/Util.swift
@@ -0,0 +1,14 @@
+//
+//  Util.swift
+//  webrtc
+//
+//  Created by 羽田 健太郎 on 2017/06/18.
+//  Copyright © 2017年 羽田 健太郎. All rights reserved.
+//
+
+import Foundation
+
+
+class Util{
+
+}
